9d42892a71b0b3d5f9da2f8c0512f70b2a4a6858
ittipu <tipu3001@gmail.com>
2020-06-24 11:17:09 +0600

    New File

---
+++ HTTP/DHT11_With_NodeMCU_PHP_MySQL/DHT11 with nodmcu php mysql database/DHT11_with_nodmcu_php_mysql_database/DHT11_with_nodmcu_php_mysql_database.ino
@@ -0,0 +1,109 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-mysql-database-php/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+*/
+
+
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClient.h>
+#include "DHT.h"
+#define DHTPIN D1  
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+int localHum = 0;
+int localTemp = 0;
+
+
+// Replace with your network credentials
+const char* ssid     = "Tipu";
+const char* password = "tipu1234";
+
+// REPLACE with your Domain name and URL path or IP address with path
+const char* serverName = "http:192.168.0.102/post_esp_data.php";
+
+// Keep this API Key value to be compatible with the PHP code provided in the project page. 
+// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
+String apiKeyValue = "tPmAT5Ab3j7F9";
+
+String sensorName = "BME280";
+String sensorLocation = "Office";
+
+/*#include <SPI.h>
+#define BME_SCK 18
+#define BME_MISO 19
+#define BME_MOSI 23
+#define BME_CS 5*/
+
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+  //Check WiFi connection status
+  if(WiFi.status()== WL_CONNECTED){
+    HTTPClient http;
+    
+    // Your Domain name with URL path or IP address with path
+    http.begin(serverName);
+    
+    // Specify content-type header
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    
+    // Prepare your HTTP POST request data
+    String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
+                          + "&location=" + sensorLocation + "&value1=" + String(1)
+                          + "&value2=" + String(2) + "&value3=" + String(3) + "";
+    Serial.print("httpRequestData: ");
+    Serial.println(httpRequestData);
+    
+    // You can comment the httpRequestData variable above
+    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
+    //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
+
+    // Send HTTP POST request
+    int httpResponseCode = http.POST(httpRequestData);
+     
+    // If you need an HTTP request with a content type: text/plain
+    //http.addHeader("Content-Type", "text/plain");
+    //int httpResponseCode = http.POST("Hello, World!");
+    
+    // If you need an HTTP request with a content type: application/json, use the following:
+    //http.addHeader("Content-Type", "application/json");
+    //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
+        
+    if (httpResponseCode>0) {
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+    }
+    else {
+      Serial.print("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    // Free resources
+    http.end();
+  }
+  else {
+    Serial.println("WiFi Disconnected");
+  }
+  //Send an HTTP POST request every 30 seconds
+  delay(30000);  
+}
---
+++ MQTT/testMQTTwithPubSubWithMess/testMQTTwithPubSubWithMess.ino
@@ -0,0 +1,43 @@
+#include<ESP8266WiFi.h>
+WiFiClient WIFI_CLIENT;
+
+
+
+#include <PubSubClient.h>
+PubSubClient MQTT_CLIENT;
+
+
+#define sensor A0
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(sensor,INPUT);
+
+  WiFi.begin("Tipu","tipu1234");
+  while(WiFi.status()!=WL_CONNECTED){
+    delay(500);
+  }
+  Serial.println("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  
+}
+
+void reconnect(){
+  MQTT_CLIENT.setServer("192.168.0.103",1883);
+  MQTT_CLIENT.setClient(WIFI_CLIENT);
+  while(!MQTT_CLIENT.connected()){
+    Serial.println("Attempt to connect to MQTT broker");
+    MQTT_CLIENT.connect("tipu");
+    delay(3000);
+  }
+  Serial.println("MQTT connected");
+}
+
+void loop(){
+  if(!MQTT_CLIENT.connected()){
+    reconnect();
+  }
+  int value=analogRead(sensor);
+  MQTT_CLIENT.publish("tipu/iot_tutorial/from_esp8266","Hello world!");
+}
